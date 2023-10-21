3fd501ad2d246356b4c6f40f2b37d12844614f00
Alexjacsouza50 <alexsolliveira9@gmail.com>
2023-10-21 01:57:26 -0300

    novas adiçcoes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //Estudar a classe string que representa texto na aplicação
+
+        String meuNome = "Alex";
+
+
+        double salarioMinimo = 2500;
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+		System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        VALOR_DE_PI = 10.75;
     }
 }
